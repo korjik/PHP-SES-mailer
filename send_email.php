7d150870151326b42d852b73cbe0694f5b408544
Andrey Kozhokaru <andrey.kozhokaru@quickblox.com>
2012-04-13 15:36:12 +0300

    Email body fixes

--- send_email.php
+++ send_email.php
@@ -3,14 +3,19 @@ require_once('ses.php');
 $ses = new SimpleEmailService('XXXXXXXXXXXXXXX', 'XXXXXXXXXXXXXXXXXXXXXX');
 $file=$argv[1];
 
-$text_email="Дорогой $username! Текст письма в формате text/plain";
-$html_email="<strong>Дорогой $username!</strong><p> Вот текс письма в формате text/html!";
+
+
 
 $user_list= fopen($file, "r");
 while (($user_fields = fgetcsv ($user_list,",")) !== FALSE)
 {
 $username=$user_fields[0];
 $email=$user_fields[1];
+
+$text_email="Дорогой $username! Текст письма в формате text/plain";
+$html_email="<strong>Дорогой $username!</strong><p> Вот текс письма в формате text/html!";
+
+
 $m = new SimpleEmailServiceMessage();
 $m->addTo($email);
 $m->setFrom('validated@email.com');
